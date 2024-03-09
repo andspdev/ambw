afd9c3ae11edc2ace8d9efd191bd92c42d845a4d
Andreas Pandu Pamungkas <andspemail@gmail.com>
2024-03-10 01:30:02 +0700

    Delete test1-ambw directory

--- test1-ambw/assets/js/app-sw.js
+++
@@ -1,9 +0,0 @@
-if ("serviceWorker" in navigator) {
-	navigator.serviceWorker.register("./sw.js")
-    .then(function() {
-		console.log("Service worker registered!");
-	});
-}
-else {
-    console.log('Browser tidak mendukung serviceWorker.');
-}
--- test1-ambw/backend/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=test1_ambw_c14210004', 'root', '');
-} catch(\PDOException $e) {
-    die('Gagal tekoneksi ke database');
-}
