116f42eb5343e70c41813d9ec50a52961d597b39
andreas.pamungkas <andreas.pamungkas@dwp.co.id>
2024-06-25 15:28:26 +0700

    Tambah alert warning color constant

---
+++ uas/lib/constant/alert_color.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color alertWarningColor = Color.fromRGBO(255, 243, 205, 1);
+const Color alertWarningBorderColor = Color.fromRGBO(255, 230, 156, 1);
+const Color alertWarningTextColor = Color.fromRGBO(102, 77, 3, 1);
