86f4d17c6639aa76c90635f3459f7bf99167ef98
andreas.pamungkas <andreas.pamungkas@dwp.co.id>
2024-06-25 16:02:49 +0700

    Update Copyright

--- uas/lib/pages/splash_screen.dart
+++ uas/lib/pages/splash_screen.dart
@@ -19,7 +19,7 @@ class _SplashScreen extends State<SplashScreen>
     super.initState();
 
 
-    Timer(const Duration(seconds: 2), () => Navigator.pushReplacement(
+    Timer(const Duration(seconds: 3), () => Navigator.pushReplacement(
       context,
       MaterialPageRoute(builder: (context) => const PinScreen())
     ));
@@ -31,7 +31,35 @@ class _SplashScreen extends State<SplashScreen>
     return const Scaffold(
       backgroundColor: backgroundColor,
       body: Center(
-        child: Text('Ini Splash Screen'),
+        child: Column(
+          children: <Widget>
+          [
+            Spacer(),
+
+            Text('NotesTaking', style: TextStyle(
+              color: textBlack,
+              fontSize: 32,
+              fontWeight: FontWeight.bold
+            )),
+
+            Spacer(),
+
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.only(bottom: 18),
+                child: Text(
+                  'Dari C14210004',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: textBlackSmooth
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )
       ),
     );
   }
